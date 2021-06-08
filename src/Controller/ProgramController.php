ab337be5a5d046d76db1b83c2144d653180f28dd
a.dagnicourt <dagnicourt.a@gmail.com>
2021-06-08 12:19:02 +0200

    quest 08 : add category handling + cat & prog display + routing and packages fixes

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,8 +17,12 @@ class ProgramController extends AbstractController
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series'
+            'programs' => $programs
         ]);
     }
 
@@ -26,8 +31,17 @@ class ProgramController extends AbstractController
      */
     public function show(int $id): Response
     {
-        return $this->render('program/show.html.twig', [
-            'id' => $id        
-        ]);
+        $program = $this->getDoctrine()
+        ->getRepository(Program::class)
+        ->findOneBy(['id' => $id]);
+
+    if (!$program) {
+        throw $this->createNotFoundException(
+            'No program with id : '.$id.' found in program\'s table.'
+        );
+    }
+    return $this->render('program/show.html.twig', [
+        'program' => $program,
+    ]);
     }
 }
