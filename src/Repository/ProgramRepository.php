111b3759c267473c30e967647f461e6c54b6ce82
a.dagnicourt <dagnicourt.a@gmail.com>
2021-06-28 17:06:03 +0200

    quest22: add latest prog carousel on index

--- src/Repository/ProgramRepository.php
+++ src/Repository/ProgramRepository.php
@@ -19,22 +19,17 @@ class ProgramRepository extends ServiceEntityRepository
         parent::__construct($registry, Program::class);
     }
 
-    // /**
-    //  * @return Program[] Returns an array of Program objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return Program[] Returns an array of Program objects
+     */
+    
+    public function getLatest()
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
             ->orderBy('p.id', 'ASC')
             ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
         ;
     }
-    */
 
     /*
     public function findOneBySomeField($value): ?Program
